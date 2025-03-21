3062906d0120e275a3c2ebca596f44c45adc2041
Cadu <carlosevtavares@usp.br>
2025-03-21 10:30:43 -0300

    mudando frase

--- lerolero.py
+++ lerolero.py
@@ -9,9 +9,23 @@ import random
 # Cada frase é composta por três partes aleatórias; aqui,
 # listas de possibilidades para cada uma das partes.
 
-parte1 = [] 
-parte2 = []
-parte3 = []
+parte1 = [
+    "O sistema em desenvolvimento",
+    "O novo protocolo de comunicação",
+    "O algoritmo foi otimizado e"
+    ]
+
+parte2 = [
+    "possui excelente desempenho",
+    "oferece garantias de precisão acima da média",
+    "preenche uma lacuna significativa"
+    ]
+
+parte3 = [
+    "nas aplicações a que se destina",
+    "em relação às opções disponíveis no mercado",
+    ", provendo ampla vantagem competitiva a seus usuários"
+    ]
 
 lingua = int(input("Escolha a lingua: 1 - português; 2 - inglês\n"))
 
